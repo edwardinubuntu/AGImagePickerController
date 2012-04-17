1b5a75db96a7836d7fc4ef45e37d6f62402eee8d
Edward Chiang <edward@thepolydice.com>
2012-04-17 15:41:01 +0800

    Enumerate group with type as user expectation.

--- AGImagePickerController/AGIPCAlbumsController.m
+++ AGImagePickerController/AGIPCAlbumsController.m
@@ -42,197 +42,210 @@
 
 - (NSMutableArray *)assetsGroups
 {
-    if (assetsGroups == nil)
-    {
-        assetsGroups = [[NSMutableArray alloc] init];
-        [self loadAssetsGroups];
-    }
-    
-    return assetsGroups;
+  if (assetsGroups == nil)
+  {
+    assetsGroups = [[NSMutableArray alloc] init];
+    [self loadAssetsGroups];
+  }
+  
+  return assetsGroups;
 }
 
 #pragma mark - Object Lifecycle
 
 - (void)dealloc
 {
-    [tableView release];
-    [assetsGroups release];
-    
-    [super dealloc];
+  [tableView release];
+  [assetsGroups release];
+  
+  [super dealloc];
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        
-    }
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
     
-    return self;
+  }
+  
+  return self;
 }
 
 - (void)didReceiveMemoryWarning
 {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
+  // Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+  
+  // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
-    
-    [self.navigationController setToolbarHidden:YES animated:YES];
+  [super viewWillAppear:animated];
+  
+  [self.navigationController setToolbarHidden:YES animated:YES];
 }
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
-    // Fullscreen
-    if (((AGImagePickerController *)self.navigationController).shouldChangeStatusBarStyle) {
-        self.wantsFullScreenLayout = YES;
-    }
-    
-    // Setup Notifications
-    [self createNotifications];
-    
-    // Navigation Bar Items
-    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelAction:)];
-	self.navigationItem.leftBarButtonItem = cancelButton;
-	[cancelButton release];
+  [super viewDidLoad];
+  
+  // Fullscreen
+  if (((AGImagePickerController *)self.navigationController).shouldChangeStatusBarStyle) {
+    self.wantsFullScreenLayout = YES;
+  }
+  
+  // Setup Notifications
+  [self createNotifications];
+  
+  // Navigation Bar Items
+  UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelAction:)];
+  self.navigationItem.leftBarButtonItem = cancelButton;
+  [cancelButton release];
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    
-    // Destroy Notifications
-    [self destroyNotifications];
+  [super viewDidUnload];
+  
+  // Destroy Notifications
+  [self destroyNotifications];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return YES;
+  return YES;
 }
 
 #pragma mark - UITableViewDataSource Methods
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.assetsGroups.count;
-    self.title = NSLocalizedStringWithDefaultValue(@"AGIPC.Loading", nil, [NSBundle mainBundle], @"Loading...", nil);
+  return self.assetsGroups.count;
+  self.title = NSLocalizedStringWithDefaultValue(@"AGIPC.Loading", nil, [NSBundle mainBundle], @"Loading...", nil);
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-    ALAssetsGroup *group = [self.assetsGroups objectAtIndex:indexPath.row];
-    [group setAssetsFilter:[ALAssetsFilter allPhotos]];
-    NSUInteger numberOfAssets = group.numberOfAssets;
-    
-    cell.textLabel.text = [NSString stringWithFormat:@"%@", [group valueForProperty:ALAssetsGroupPropertyName]];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", numberOfAssets];
-    [cell.imageView setImage:[UIImage imageWithCGImage:[(ALAssetsGroup*)[assetsGroups objectAtIndex:indexPath.row] posterImage]]];
-	[cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-	
-    return cell;
+  static NSString *CellIdentifier = @"Cell";
+  
+  UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
+  }
+  
+  ALAssetsGroup *group = [self.assetsGroups objectAtIndex:indexPath.row];
+  [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+  NSUInteger numberOfAssets = group.numberOfAssets;
+  
+  cell.textLabel.text = [NSString stringWithFormat:@"%@", [group valueForProperty:ALAssetsGroupPropertyName]];
+  cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", numberOfAssets];
+  [cell.imageView setImage:[UIImage imageWithCGImage:[(ALAssetsGroup*)[assetsGroups objectAtIndex:indexPath.row] posterImage]]];
+  [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+  
+  return cell;
 }
 
 #pragma mark - UITableViewDelegate Methods
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
-	AGIPCAssetsController *controller = [[AGIPCAssetsController alloc] initWithAssetsGroup:[self.assetsGroups objectAtIndex:indexPath.row]];
-	[self.navigationController pushViewController:controller animated:YES];
-	[controller release];
+  [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+  
+  AGIPCAssetsController *controller = [[AGIPCAssetsController alloc] initWithAssetsGroup:[self.assetsGroups objectAtIndex:indexPath.row]];
+  [self.navigationController pushViewController:controller animated:YES];
+  [controller release];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {	
-	return 57;
+  return 57;
 }
 
 #pragma mark - Private
 
 - (void)loadAssetsGroups
 {
-    [self.assetsGroups removeAllObjects];
+  [self.assetsGroups removeAllObjects];
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-        
-        @autoreleasepool {
-            
-            void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop) 
-            {
-                if (group == nil) 
-                {
-                    return;
-                }
-                
-                [self.assetsGroups addObject:group];
-                
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self reloadData];
-                });
-            };
-            
-            void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *error) {
-                NSLog(@"A problem occured. Error: %@", error.localizedDescription);
-                [((AGImagePickerController *)self.navigationController) performSelector:@selector(didFail:) withObject:error];
-            };	
-            
-            [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupAll
-                                   usingBlock:assetGroupEnumerator 
-                                 failureBlock:assetGroupEnumberatorFailure];
-            
+    @autoreleasepool {
+      
+      void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop) 
+      {
+        if (group == nil) 
+        {
+          return;
         }
-        
-    });
+        if (![self.assetsGroups containsObject:group]) {
+          [self.assetsGroups addObject:group];
+        }
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+          [self reloadData];
+        });
+      };
+      
+      void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *error) {
+        NSLog(@"A problem occured. Error: %@", error.localizedDescription);
+        [((AGImagePickerController *)self.navigationController) performSelector:@selector(didFail:) withObject:error];
+      };	
+      
+      [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupPhotoStream
+                                                                    usingBlock:assetGroupEnumerator 
+                                                                  failureBlock:assetGroupEnumberatorFailure];
+      [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
+                                                                    usingBlock:assetGroupEnumerator 
+                                                                  failureBlock:assetGroupEnumberatorFailure];
+      [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupAlbum
+                                                                    usingBlock:assetGroupEnumerator 
+                                                                  failureBlock:assetGroupEnumberatorFailure];
+      [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupEvent
+                                                                    usingBlock:assetGroupEnumerator 
+                                                                  failureBlock:assetGroupEnumberatorFailure];
+      [[AGImagePickerController defaultAssetsLibrary] enumerateGroupsWithTypes:ALAssetsGroupFaces
+                                                                    usingBlock:assetGroupEnumerator 
+                                                                  failureBlock:assetGroupEnumberatorFailure];
+      
+    }
+    
+  });
 }
 
 - (void)reloadData
 {
-    [self.tableView reloadData];
-    self.title = NSLocalizedStringWithDefaultValue(@"AGIPC.Albums", nil, [NSBundle mainBundle], @"Albums", nil);
+  [self.tableView reloadData];
+  self.title = NSLocalizedStringWithDefaultValue(@"AGIPC.Albums", nil, [NSBundle mainBundle], @"Albums", nil);
 }
 
 - (void)cancelAction:(id)sender
 {
-    [((AGImagePickerController *)self.navigationController) performSelector:@selector(didCancelPickingAssets)];
+  [((AGImagePickerController *)self.navigationController) performSelector:@selector(didCancelPickingAssets)];
 }
 
 #pragma mark - Notifications
 
 - (void)createNotifications
 {
-    [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(didChangeLibrary:) 
-                                                 name:ALAssetsLibraryChangedNotification 
-                                               object:[AGImagePickerController defaultAssetsLibrary]];
+  [[NSNotificationCenter defaultCenter] addObserver:self 
+                                           selector:@selector(didChangeLibrary:) 
+                                               name:ALAssetsLibraryChangedNotification 
+                                             object:[AGImagePickerController defaultAssetsLibrary]];
 }
 
 - (void)destroyNotifications
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self 
-                                                    name:ALAssetsLibraryChangedNotification 
-                                                  object:[AGImagePickerController defaultAssetsLibrary]];
+  [[NSNotificationCenter defaultCenter] removeObserver:self 
+                                                  name:ALAssetsLibraryChangedNotification 
+                                                object:[AGImagePickerController defaultAssetsLibrary]];
 }
 
 - (void)didChangeLibrary:(NSNotification *)notification
 {
-    [self loadAssetsGroups];
+  [self loadAssetsGroups];
 }
 
 @end
